be711f067e674d259a3c4cf94568e8f73e9bd62a
DangerDanDev <scyth3s@gmail.com>
2021-04-16 13:17:09 -0700

    Moved over to a grid layout with a List of 4 camera inputs

--- App.py
+++ App.py
@@ -15,27 +15,35 @@ class App:
 
         self.window.title('Webcam test!')
 
-        # Uncomment this on my desktop to load a camera from a pre-saved video file
-        self.camera = Camera(r'C:\Users\scyth\Documents\Programming\PythonDemos\pexels-how-far-from-home-5592502.mp4')
-        # Uncomment this to load a camera from the webcam on my laptop
-        # self.camera = Camera(0)
+        self.cameras = list()
+        self.camera_canvases = list()
 
-        self.camera2 = Camera(r'C:\Users\scyth\Documents\Programming\PythonDemos\pexels-how-far-from-home-5592502.mp4')
-        # self.camera2 = Camera(1)
+        width = 133
+        height = 100
+        x = 0
+        y = 0
 
-        self.canvas = CameraCanvas.CameraCanvas(self.window, self.camera, width=133, height=100)
-        self.canvas.pack(side='top')
+        for i in range(0,4):
+            self.cameras.append(Camera(r'C:\Users\scyth\Documents\Programming\PythonDemos\pexels-how-far-from-home-5592502.mp4'))
+            self.camera_canvases.append(CameraCanvas.CameraCanvas(self.window, self.cameras[i], width=width, height=height))
 
-        self.canvas2 = CameraCanvas.CameraCanvas(self.window, self.camera2, width=133, height=100)
-        self.canvas2.pack()
+        # leftmost camera takes up the left third of the screen
+        self.camera_canvases[0].grid(column=0, row=0, rowspan=2)
+
+        # Middle 2 cameras split (top/bottom) the middle third of the screen
+        self.camera_canvases[1].grid(column=1, row=0)
+        self.camera_canvases[2].grid(column=1, row=1)
+
+        # The right camera takes the right third of the screen
+        self.camera_canvases[3].grid(column=2, row=0, rowspan=2)
 
         self.update()
         self.window.mainloop()
 
     def update(self):
 
-        self.canvas.update()
-        self.canvas2.update()
+        for canvas in self.camera_canvases:
+            canvas.update()
 
         self.window.after(self.delay, self.update)
 
